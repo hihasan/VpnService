3ddc4ffb9baa6941d555ec38b329bc040fa759be
Arabi <incognito2170@gmail.com>
2019-03-08 02:12:25 +0600

    Added account choosing fragment for sign in.

--- app/src/main/java/com/poc/vpnservice/fragment/LandingPageStatusTabFragment.java
+++ app/src/main/java/com/poc/vpnservice/fragment/LandingPageStatusTabFragment.java
@@ -5,20 +5,28 @@ package com.poc.vpnservice.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.poc.vpnservice.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LandingPageStatusTabFragment extends Fragment {
     private View view;
+    private AppCompatSpinner accounts_spinner;
+    private List<String> accounts_list;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.landing_page_status_tab_fragment, container, false);
-
         return view;
     }
 
@@ -27,6 +35,26 @@ public class LandingPageStatusTabFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        accounts_spinner = view.findViewById(R.id.sp_accounts);
+        accounts_list = new ArrayList<>();
+        accounts_list.add("arabi@abc.com");
+        accounts_list.add("nadim@d.com");
+        accounts_list.add("john@xyz.com");
+
+        ArrayAdapter adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, accounts_list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accounts_spinner.setAdapter(adapter);
+        accounts_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String emailId = accounts_list.get(i);
+                //Toast.makeText(getActivity(), "Selected Account: " + emailId, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
